008a1d5141ae422c34662c8f78618aef5fb73e85
simiendo <simiendo.as@gmail.com>
2017-10-09 22:29:38 +0300

    Added edit.php, needs fixing

---
+++ edit.php
@@ -0,0 +1,79 @@
+<?php
+
+include "db-connect.php";
+$id = $_GET['id'];
+$sql = "SELECT FROM animals WHERE id=$id";
+$select=mysqli_query($conn, $sql);
+$row = mysqli_fetch_array($select);
+
+    $nume = $row["word"];
+
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Customize</title>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <link rel=stylesheet" href="bootstrap/css/bootstrap-theme.min.css">
+</head>
+<body>
+<div class="container">
+
+
+    <div class="page-header">
+        <h1 class="h2">Edit <a class="btn btn-default" href="index.php"> Toate Animalele </a></h1>
+    </div>
+
+    <div class="clearfix"></div>
+
+    <form method="post" enctype="multipart/form-data" class="form-horizontal">
+
+
+        <?php
+        if(isset($errMSG)){
+            ?>
+            <div class="alert alert-danger">
+                <span class="glyphicon glyphicon-info-sign"></span> &nbsp; <?php echo $errMSG; ?>
+            </div>
+            <?php
+        }
+        ?>
+
+
+        <table class="table table-bordered table-responsive">
+
+            <tr>
+                <td><label class="control-label">Nume </label></td>
+                <td><input class="form-control" type="text" name="Word" value="<?php echo $nume; ?>" required /></td>
+            </tr>
+
+
+            <tr>
+                <td><label class="control-label">Imagine </label></td>
+                <td>
+                    <p><img src='get-image.php?id=<?php echo $id ?>' height="150" width="150" /></p>
+                    <input class="input-group" type="file" name="user_image" accept="image/*" />
+                </td>
+            </tr>
+
+            <tr>
+                <td colspan="2"><button type="submit" name="btn_save_updates" class="btn btn-default">
+                        <span class="glyphicon glyphicon-save"></span> Update
+                    </button>
+
+                    <a class="btn btn-default" href="database.php"> <span class="glyphicon glyphicon-backward"></span> Cancel </a>
+
+                </td>
+            </tr>
+
+        </table>
+
+    </form>
+</body>
+
+</html>
