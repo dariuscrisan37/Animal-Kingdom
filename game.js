1519954f44808414db5489573908f9be45ced6c5
simiendo <simiendo.as@gmail.com>
2017-10-16 16:02:22 +0300

    [Work in Progress] Saved it, in case i mess up.

--- game.js
+++ game.js
@@ -1,25 +1,33 @@
+
+div_numeanimal = document.getElementById("nume-animal");
+div_used = document.getElementById('used');
+div_litere = document.getElementById("litere");
 var numeanimal = document.querySelector('#nume-animal').innerHTML;
-    document.querySelector('#nume-animal').innerHTML = '';
+
+document.querySelector('#nume-animal').innerHTML = '';
 var length = numeanimal.length;
+
 for (var i = 0; i < length; i++) {
-    var span = document.createElement('span');
-    span.className = 'coloredText ' + (i + 1);
-    span.innerHTML = numeanimal[i];
-    document.getElementById("nume-animal").appendChild(span);
+    var box = document.createElement('div');
+    box.id = 'letter_' + i;
+
+    if(numeanimal[i]==' ') div_numeanimal.innerHTML += '&nbsp;&nbsp;&nbsp;&nbsp;';
+    else box.className += 'box letter';
+
+    // box.innerHTML = numeanimal[i];
+
+    div_numeanimal.appendChild(box);
 
 }
-
 //alphabetul declarat in variabila
 var alfabet = 'ABCDEFGHIJKLMNOPQRSTUVXYZ';
 
 //butoanele create in javascript
 function letters(){
-    div_used = document.getElementById('used');
-    div_litere = document.getElementById("litere");
     for(var i=0; i<alfabet.length; i++){
         var lit = document.createElement('div');
         lit.innerHTML = alfabet[i];
-        lit.className = "litere";
+        lit.className = "btnlitere";
         lit.onclick = function(){selectLitera(this);};
         div_litere.appendChild(lit);
     }
@@ -34,5 +42,11 @@ function selectLitera(selected){
     lit.innerHTML = selected.innerHTML;
     lit.className = 'litUsed';
     div_used.appendChild(lit);
+
+
+    var letter = selected.innerHTML;
+    for(var i=0; i<numeanimal.length; i++){
+        if (numeanimal[i] == letter) document.getElementById('letter_' + i).innerHTML = letter;
+    }
 }
 // selectLitera(selected);
